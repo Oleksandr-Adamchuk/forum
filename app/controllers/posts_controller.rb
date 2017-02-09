1f405a37b1c88467d27fe9a1c5f413b44ff1e5f4
Oleksandr Adamchuk <stranger.if.2014@gmail.com>
2017-02-09 14:05:57 +0000

    Add post new view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,19 @@ class PostsController < ApplicationController
   def index
     
   end
+  
+  def new
+    @posts = Post.new
+  end
+  
+  def create
+    @posts = Post.new(post_params)
+  end
+  
+  
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+  
 end
