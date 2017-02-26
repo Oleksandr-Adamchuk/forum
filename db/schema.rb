498f9c20e62ddedf79ef986036e9f77de1c28507
Oleksandr Adamchuk <stranger.if.2014@gmail.com>
2017-02-26 11:45:05 +0000

    Finish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222044732) do
+ActiveRecord::Schema.define(version: 20170226112532) do
 
   create_table "categories", force: :cascade do |t|
     t.integer  "user_id"
@@ -33,9 +33,10 @@ ActiveRecord::Schema.define(version: 20170222044732) do
     t.string   "username"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "remember_token"
+    t.boolean  "admin",           default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -4,7 +4,8 @@ namespace :db do
     User.create!(username: "Iren",
                  email: "iren.mb@gmail.org",
                  password: "123123",
-                 password_confirmation: "123123")
+                 password_confirmation: "123123",
+                 admin: true)
     99.times do |n|
       name  = Faker::Name.name
       email = "example-#{n+1}@gmail.org"
